948772df4566cd7c04eb53afb96bc24932a36a9f
Ewy Rostam <ewy.rost@gmail.com>
2024-04-17 18:11:37 +0200

    Add db context to program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Reflection;
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var config = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json", false, true)
+.AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+.Build();
 // Add services to the container.
+builder.Services.AddDbContext<GreetingContext>(options =>
+    options.UseSqlServer(config.GetConnectionString("GreetingContext") ??
+    throw new InvalidOperationException("Connection string 'GreetingContext' not found.")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,11 +20,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
