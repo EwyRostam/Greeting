4d54abc86bd9eb377bf5b90df82b2cc119dbbd77
Ewy Rostam <ewy.rost@gmail.com>
2024-04-17 18:15:15 +0200

    Create endpoint for posting greetings

--- Backend/Controllers/GreetingsController.cs
+++ Backend/Controllers/GreetingsController.cs
@@ -1,3 +1,5 @@
+using Backend.Data;
+using Backend.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
@@ -6,6 +8,20 @@ namespace Backend.Controllers
     [Route("[controller]")]
     public class GreetingsController : ControllerBase
     {
-        
+        private readonly GreetingContext _context;
+
+        public GreetingsController(GreetingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Greeting>> CreateGreeting(Greeting greeting)
+        {
+            var greetingToAdd = new Greeting(){Content = greeting.Content};
+            await _context.Greetings.AddAsync(greetingToAdd);
+            await _context.SaveChangesAsync();
+            return greetingToAdd;
+        }
     }
 }
